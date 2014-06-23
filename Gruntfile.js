143cc0edf763ffb1d613b956710867331d2a027e
Francesco Trotta <github@fasttime.org>
2014-06-24 00:33:46 +0200

    Added NO_IE output and test cases

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,6 @@ module.exports = function(grunt) {
         curly: true,
         eqeqeq: true,
         immed: true,
-        latedef: true,
         newcap: true,
         noarg: true,
         quotmark: true,
--- test/jsfuck_test.js
+++ test/jsfuck_test.js
@@ -1,47 +1,77 @@
-/*jshint -W061 */
 'use strict';
 
-var JSFuck = require('../jsfuck.js').JSFuck,
-	test_encode = function (test, value) {
-		var encoded = JSFuck.encode(value),
-			unencoded = eval(encoded);
-
-		test.strictEqual(value, unencoded, 'encoding "' + value + '" failed');
-	};
-var MIN = 32, MAX = 127;
+var JSFuck = require('../jsfuck.js').JSFuck;
 var fs = require('fs');
 
-exports['integration'] = {
-	'test': function(test) {
-		var file = fs.openSync('output.txt', 'w+');
+function createTest(input)
+{
+    exports['encode_tests']['encode "' + input + '"'] = testEncode.bind(undefined, input);
+}
 
-		for (var i = MIN; i < MAX; i++) {
-			var c = String.fromCharCode(i),
-				encoded = JSFuck.encode(c);
-			fs.writeSync(file, '`' + c + '` ' + encoded.length + '\n');
-		}
+function testEncode(value, test)
+{
+    var encoded;
+    
+    encoded = JSFuck.encode(value);
+    var unencodedDefault = eval(encoded);
+    test.strictEqual(
+        value,
+        unencodedDefault,
+        'encoding "' + value + '" with compatibility DEFAULT failed'
+        );
+    
+    encoded = JSFuck.encode(value, false, 'NO_IE');
+    var unencodedNoIE = eval(encoded);
+    test.strictEqual(
+        value,
+        unencodedNoIE,
+        'encoding "' + value + '" with compatibility NO_IE failed'
+        );
+    test.ok(
+        unencodedNoIE.length <= unencodedDefault.length,
+        'encoding "' + value + '" with compatibility NO_IE is suboptimal'
+        );
+    
+    test.done();
+}
 
-		fs.closeSync(file);
-		test.done();
-	}
+var MIN = 32, MAX = 127;
+
+exports['integration'] =
+{
+    'test': function (test)
+    {
+        var file = fs.openSync('output.txt', 'w+');
+
+        fs.writeSync(file, '    DEFAULT  NO_IE\n    ------- -------\n');
+        for (var i = MIN; i < MAX; ++i)
+        {
+            var c = String.fromCharCode(i);
+            var encodedDefault = JSFuck.encode(c, false, 'DEFAULT');
+            var encodedNoIE = JSFuck.encode(c, false, 'NO_IE');
+            fs.writeSync(
+                file,
+                '`' + c + '`' +
+                ('        ' + encodedDefault.length).slice(-8) +
+                ('        ' + encodedNoIE.length).slice(-8) +
+                '\n'
+                );
+        }
+
+
+        fs.closeSync(file);
+        test.done();
+    }
 };
 
-var test = function (c, test) {
-	test_encode(test, c);
-	test.done();
-};
-
-var createTest = function (input) {
-	exports['encode_tests']['encode "'+input+'"'] = test.bind(undefined, input);
-};
-
-exports['encode_tests'] = {};
+exports['encode_tests'] = { };
 
 createTest('false');
 createTest('falsefalsetrue');
 createTest('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 createTest('abcdefghijklmnopqrstuvwxyz');
 
-for(var i=MIN; i<MAX ;i++) {
-	createTest(String.fromCharCode(i));
+for (var i = MIN; i < MAX; ++i)
+{
+    createTest(String.fromCharCode(i));
 }
