744d29a837ef57ccdacd84a0a363afbbc8d025fc
Francesco Trotta <noone@nos-macbook-air.fritz.box>
2014-06-26 00:43:33 +0200

    Beautified definition syntax
    Definitions may now include blanks for better readability.
    Zero padding constant dropped in favor of “[]”.
    Minor performance optimizations.

--- jscrewit.js
+++ jscrewit.js
@@ -18,15 +18,15 @@
         // position on different browsers. The number after "FBP_" is the maximum character
         // overhead; the suffix "_NS" indicates that the constant does not evaluate to a string or
         // an array.
-        FBP_9_NS:       '[false][+!(false+ANY_FUNCTION)["40"]]',
+        FBP_9_NS:       '[false][+!(false + ANY_FUNCTION)["40"]]',
         FBP_10:
         {
-            NO_IE:      '[0]+FBP_9_NS'
+            NO_IE:      '[0] + FBP_9_NS'
         },
-        FBP_15:         'FHP_5_NS+[0]+FBP_9_NS',
-        FBP_16:         'FHP_3_NS+[true]+FBP_9_NS',
-        FBP_18:         'FHP_5_NS+[true]+FBP_9_NS',
-        FBP_20:         'FHP_5_NS+[0]+false+FBP_9_NS',
+        FBP_15:         'FHP_5_NS + [0] + FBP_9_NS',
+        FBP_16:         'FHP_3_NS + [true] + FBP_9_NS',
+        FBP_18:         'FHP_5_NS + [true] + FBP_9_NS',
+        FBP_20:         'FHP_5_NS + [0] + false + FBP_9_NS',
 
         // Function header padding constants: prepended to a function to align the header at the
         // same position on different browsers. The number after "FHP_" is the maximum character
@@ -34,16 +34,16 @@
         // an array.
         FHP_3_NS:
         {
-            DEFAULT:    '+((ANY_FUNCTION+[])[0]+"10")',
+            DEFAULT:    '+((ANY_FUNCTION + [])[0] + "10")',
             NO_IE:      'NaN'
         },
         FHP_5_NS:
         {
-            DEFAULT:    '!!+((ANY_FUNCTION+[])[0]+1)',
+            DEFAULT:    '!!+((ANY_FUNCTION + [])[0] + 1)',
             NO_IE:      'false'
         },
-        FHP_7:          'FHP_3_NS+[true]',
-        FHP_8:          'FHP_5_NS+[NaN]',
+        FHP_7:          'FHP_3_NS + [true]',
+        FHP_8:          'FHP_5_NS + [NaN]',
         
         // Regular padding constants: The number after "RP_" is the character overhead; the suffix
         // "_NS" indicates that the constant does not evaluate to a string or an array.
@@ -53,9 +53,6 @@
         RP_5:           '[false]',
         RP_5_NS:        'false',
         RP_6:           '"0false"',
-        
-        // Zero padding constant: used to convert an adjacent expression into a string.
-        ZP:             '[]'
     };
     
     var CONSTRUCTORS =
@@ -65,7 +62,7 @@
         Function:   'ANY_FUNCTION',
         Number:     '(0)',
         RegExp:     'Function("return/false/")()',
-        String:     '("")'
+        String:     '("")',
     };
     
     var CHARACTERS =
@@ -73,33 +70,33 @@
         'a':            '"false"[1]',
         'b':
         {
-            DEFAULT:    '(FHP_8+Number)["20"]',
-            NO_IE:      '(ZP+Number)["12"]'
+            DEFAULT:    '(FHP_8 + Number)["20"]',
+            NO_IE:      '(Number + [])["12"]'
         },
         'c':
         {
-            DEFAULT:    '(FHP_7+ANY_FUNCTION)["10"]',
-            NO_IE:      '(ZP+ANY_FUNCTION)[3]'
+            DEFAULT:    '(FHP_7 + ANY_FUNCTION)["10"]',
+            NO_IE:      '(ANY_FUNCTION + [])[3]'
         },
         'd':            '"undefined"[2]',
         'e':            '"true"[3]',
         'f':            '"false"[0]',
         'g':
         {
-            DEFAULT:    '(FHP_7+String)["21"]',
-            NO_IE:      '(RP_6+String)["20"]'
+            DEFAULT:    '(FHP_7 + String)["21"]',
+            NO_IE:      '(RP_6 + String)["20"]'
         },
         'h':            '(101)["toString"]("21")[1]',
-        'i':            '(RP_5+undefined)["10"]',
-        'j':            '(Function("return{}")()+[])["10"]',
+        'i':            '(RP_5 + undefined)["10"]',
+        'j':            '(Function("return{}")() + [])["10"]',
         'k':            '(20)["toString"]("21")',
         'l':            '"false"[2]',
-        'm':            '(RP_6+Function())["20"]',
+        'm':            '(RP_6 + Function())["20"]',
         'n':            '"undefined"[1]',
         'o':
         {
-            DEFAULT:    '(FHP_5_NS+ANY_FUNCTION)["11"]',
-            NO_IE:      '(RP_4_NS+ANY_FUNCTION)["10"]'
+            DEFAULT:    '(FHP_5_NS + ANY_FUNCTION)["11"]',
+            NO_IE:      '(RP_4_NS + ANY_FUNCTION)["10"]'
         },
         'p':            '(211)["toString"]("31")[1]',
         'q':            '(212)["toString"]("31")[1]',
@@ -107,37 +104,37 @@
         's':            '"false"[3]',
         't':            '"true"[0]',
         'u':            '"undefined"[0]',
-        'v':            '(FBP_15+ANY_FUNCTION)["40"]',
+        'v':            '(FBP_15 + ANY_FUNCTION)["40"]',
         'w':            '(32)["toString"]("33")',
         'x':            '(101)["toString"]("34")[1]',
-        'y':            '(RP_3_NS+[Infinity])["10"]',
+        'y':            '(RP_3_NS + [Infinity])["10"]',
         'z':            '(35)["toString"]("36")',
 
         'A':
         {
-            DEFAULT:    '(FHP_3_NS+Array)["12"]',
-            NO_IE:      '(RP_1_NS+Array)["10"]'
+            DEFAULT:    '(FHP_3_NS + Array)["12"]',
+            NO_IE:      '(RP_1_NS + Array)["10"]'
         },
         'B':
         {
-            DEFAULT:    '(FHP_3_NS+Boolean)["12"]',
-            NO_IE:      '(RP_1_NS+Boolean)["10"]'
+            DEFAULT:    '(FHP_3_NS + Boolean)["12"]',
+            NO_IE:      '(RP_1_NS + Boolean)["10"]'
         },
         'C':            'Function("return escape")()(""["italics"]())[2]',
         'D':            'Function("return escape")()("}")[2]',
         'E':
         {
-            DEFAULT:    '(FHP_8+RegExp)["20"]',
-            NO_IE:      '(ZP+RegExp)["12"]'
+            DEFAULT:    '(FHP_8 + RegExp)["20"]',
+            NO_IE:      '(RegExp + [])["12"]'
         },
         'F':
         {
-            DEFAULT:    '(FHP_3_NS+Function)["12"]',
-            NO_IE:      '(RP_1_NS+Function)["10"]'
+            DEFAULT:    '(FHP_3_NS + Function)["12"]',
+            NO_IE:      '(RP_1_NS + Function)["10"]'
         },
         'G':
         {
-            NO_IE:      '(RP_5_NS+Function("return Date")()())["30"]' // not for IE ≤ 10
+            NO_IE:      '(RP_5_NS + Function("return Date")()())["30"]' // not for IE ≤ 10
         },
     //  'H':    ,
         'I':            '"Infinity"[0]',
@@ -146,35 +143,35 @@
     //  'L':    ,
         'M':
         {
-            NO_IE:      '(RP_4_NS+Function("return Date")()())["30"]' // not for IE ≤ 10
+            NO_IE:      '(RP_4_NS + Function("return Date")()())["30"]' // not for IE ≤ 10
         },
         'N':            '"NaN"[0]',
-        'O':            '(RP_3_NS+Function("return{}")())["11"]',
+        'O':            '(RP_3_NS + Function("return{}")())["11"]',
     //  'P':    ,
     //  'Q':    ,
         'R':
         {
-            DEFAULT:    '(FHP_3_NS+RegExp)["12"]',
-            NO_IE:      '(RP_1_NS+RegExp)["10"]'
+            DEFAULT:    '(FHP_3_NS + RegExp)["12"]',
+            NO_IE:      '(RP_1_NS + RegExp)["10"]'
         },
         'S':
         {
-            DEFAULT:    '(FHP_3_NS+String)["12"]',
-            NO_IE:      '(RP_1_NS+String)["10"]'
+            DEFAULT:    '(FHP_3_NS + String)["12"]',
+            NO_IE:      '(RP_1_NS + String)["10"]'
         },
         'T':
         {
-            NO_IE:      '(RP_3_NS+Function("return Date")()())["30"]' // not for IE ≤ 10
+            NO_IE:      '(RP_3_NS + Function("return Date")()())["30"]' // not for IE ≤ 10
         },
-        'U':            '(RP_3_NS+Function("return{}")()["toString"]["call"]())["11"]',
+        'U':            '(RP_3_NS + Function("return{}")()["toString"]["call"]())["11"]',
     //  'V':    ,
     //  'W':    ,
     //  'X':    ,
     //  'Y':    ,
     //  'Z':    ,
 
-        '\n':           '(ZP+Function())["23"]',
-        ' ':            '(FHP_3_NS+ANY_FUNCTION)["11"]',
+        '\n':           '(Function() + [])["23"]',
+        ' ':            '(FHP_3_NS + ANY_FUNCTION)["11"]',
     //  '!':    ,
         '"':            '""["fontcolor"]()["12"]',
     //  '#':    ,
@@ -182,47 +179,47 @@
         '%':            'Function("return escape")()(ANY_FUNCTION)["20"]',
     //  '&':    ,
     //  '\'':   ,
-        '(':            '(FHP_5_NS+ANY_FUNCTION)["20"]',
+        '(':            '(FHP_5_NS + ANY_FUNCTION)["20"]',
         ')':
         {
-            DEFAULT:    '(FHP_5_NS+ANY_FUNCTION)["21"]',
-            NO_IE:      '(RP_4_NS+ANY_FUNCTION)["20"]'
+            DEFAULT:    '(FHP_5_NS + ANY_FUNCTION)["21"]',
+            NO_IE:      '(RP_4_NS + ANY_FUNCTION)["20"]'
         },
     //  '*':    ,
-        '+':            '(+"1e100"+[])[2]',
-        ',':            '([]["slice"]["call"]("false")+[])[1]',
-        '-':            '(+".0000000001"+[])[2]',
-        '.':            '(+"11e20"+[])[1]',
+        '+':            '(+"1e100" + [])[2]',
+        ',':            '([]["slice"]["call"]("false") + [])[1]',
+        '-':            '(+".0000000001" + [])[2]',
+        '.':            '(+"11e20" + [])[1]',
         '/':            '"0false"["italics"]()["10"]',
-        ':':            '(ZP+RegExp())[3]',
+        ':':            '(RegExp() + [])[3]',
     //  ';':    ,
         '<':            '""["italics"]()[0]',
         '=':            '""["fontcolor"]()["11"]',
         '>':            '""["italics"]()[2]',
-        '?':            '(ZP+RegExp())[2]',
+        '?':            '(RegExp() + [])[2]',
     //  '@':    ,
         '[':
         {
-            DEFAULT:    '(FBP_20+ANY_FUNCTION)["40"]',
-            NO_IE:      '(FBP_10+ANY_FUNCTION)["30"]'
+            DEFAULT:    '(FBP_20 + ANY_FUNCTION)["40"]',
+            NO_IE:      '(FBP_10 + ANY_FUNCTION)["30"]'
         },
     //  '\\':   ,
         ']':
         {
-            DEFAULT:    '(FBP_18+ANY_FUNCTION)["50"]',
-            NO_IE:      '(FBP_9_NS+ANY_FUNCTION)["41"]'
+            DEFAULT:    '(FBP_18 + ANY_FUNCTION)["50"]',
+            NO_IE:      '(FBP_9_NS + ANY_FUNCTION)["41"]'
         },
     //  '^':    ,
     //  '_':    ,
     //  '`':    ,
-        '{':            '(FHP_3_NS+ANY_FUNCTION)["21"]',
+        '{':            '(FHP_3_NS + ANY_FUNCTION)["21"]',
     //  '|':    ,
         '}':
         {
-            DEFAULT:    '(FBP_16+ANY_FUNCTION)["50"]',
-            NO_IE:      '(FBP_9_NS+ANY_FUNCTION)["43"]'
+            DEFAULT:    '(FBP_16 + ANY_FUNCTION)["50"]',
+            NO_IE:      '(FBP_9_NS + ANY_FUNCTION)["43"]'
         },
-    //  '~':
+    //  '~':    ,
     };
     
     var SIMPLE =
@@ -231,8 +228,10 @@
         'true':         '!![]',
         'undefined':    '[][[]]',
         'NaN':          '+[![]]',
-        'Infinity':     '+"1e1000"'
+        'Infinity':     '+"1e1000"',
     };
+    
+    var simplePattern;
     
     function Encoder(compatibility)
     {
@@ -287,10 +286,12 @@
         
         replace: function (expr)
         {
-            expr = expr.replace(/"(.*?)"/g, stringReplacer.bind(this));
-            expr = expr.replace(/[\$A-Z_a-z][\$0-9A-Z_a-z]*/g, literalReplacer.bind(this));
-            expr = expr.replace(/[0-9]+/g, numberReplacer);
-            return expr;
+            var replacement =
+                expr.replace(
+                /([0-9]+)|("(.*?)")|( +)|([$A-Z_a-z][$0-9A-Z_a-z]*)|[^!()+[\]]/g,
+                this.replaceToken || (this.replaceToken = replaceToken.bind(this))
+                );
+            return replacement;
         },
     
         replaceAndCache: function (expr)
@@ -399,7 +400,11 @@
             var fullLevel;
             if (string)
             {
-                var regExp = new RegExp(Object.keys(SIMPLE).join('|') + '|[^]', 'g');
+                if (!simplePattern)
+                {
+                    simplePattern = Object.keys(SIMPLE).join('|') + '|[^]';
+                }
+                var regExp = new RegExp(simplePattern, 'g');
                 var match;
                 while (match = regExp.exec(string))
                 {
@@ -416,7 +421,7 @@
                     var level = tokenValue.level;
                     if (tokenValue.level === undefined)
                     {
-                        var type = typeof(eval(String(tokenValue)));
+                        var type = typeof(eval(tokenValue + ''));
                         switch (type)
                         {
                         case 'string':
@@ -458,7 +463,7 @@
                     else
                     {
                         fullLevel = level;
-                        result = String(tokenValue);
+                        result = tokenValue + '';
                     }
                 }
             }
@@ -499,7 +504,7 @@
     {
         for (var number = 0; number < 10; ++number)
         {
-            var digit = String(number);
+            var digit = number + '';
             CHARACTERS[digit] = encodeDigit(digit);
         }
     }
@@ -538,59 +543,99 @@
         return false;
     }
     
-    function literalReplacer(literal, offset, total)
+    function isFollowedByLeftSquareBracket(expr, offset)
+    {
+        for (;;)
+        {
+            var character = expr[offset++];
+            if (character === '[')
+            {
+                return true;
+            }
+            if (character !== ' ')
+            {
+                return false;
+            }
+        }
+    }
+    
+    function isPrecededByOperator(expr, offset)
+    {
+        for (;;)
+        {
+            var character = expr[--offset];
+            if (character === '+' || character === '!')
+            {
+                return true;
+            }
+            if (character !== ' ')
+            {
+                return false;
+            }
+        }
+    }
+    
+    function replaceToken(wholeMatch, number, quotedString, string, space, literal, offset, expr)
     {
         var replacement;
-        if (literal in CONSTANTS)
+        if (number)
         {
-            replacement = this.resolveConstant(literal);
+            replacement = encodeDigit(number[0]);
+            var length = number.length;
+            for (var index = 1; index < length; ++index)
+            {
+                replacement += '+[' + encodeDigit(number[index]) + ']';
+            }
+            if (length > 1)
+            {
+                replacement = '+(' + replacement + ')';
+            }
+            if (isPrecededByOperator(expr, offset))
+            {
+                replacement = '(' + replacement + ')';
+            }
         }
-        else if (literal in CONSTRUCTORS)
+        else if (quotedString)
         {
-            replacement = this.resolveConstructor(literal);
+            replacement =
+                this.resolveString(
+                string,
+                isPrecededByOperator(expr, offset) ||
+                isFollowedByLeftSquareBracket(expr, offset + wholeMatch.length)
+                );
         }
-        else if (literal in SIMPLE)
+        else if (space)
         {
-            replacement = this.resolveSimple(literal);
+            replacement = '';
+        }
+        else if (literal)
+        {
+            if (literal in CONSTANTS)
+            {
+                replacement = this.resolveConstant(literal);
+            }
+            else if (literal in CONSTRUCTORS)
+            {
+                replacement = this.resolveConstructor(literal);
+            }
+            else if (literal in SIMPLE)
+            {
+                replacement = this.resolveSimple(literal);
+            }
+            else
+            {
+                throw new SyntaxError('Undefined literal ' + literal);
+            }
+            if (isPrecededByOperator(expr, offset) && hasOuterPlus(replacement))
+            {
+                replacement = '(' + replacement + ')';
+            }
         }
         else
         {
-            throw new SyntaxError('Undefined literal ' + literal);
-        }
-        if (total[offset - 1] === '+' && hasOuterPlus(replacement))
-        {
-            replacement = '(' + replacement + ')';
+            throw new SyntaxError('Unexpected character ' + wholeMatch);
         }
         return replacement;
-    }
-    
-    function numberReplacer(number, offset, total)
-    {
-        var replacement = encodeDigit(number[0]);
-        var length = number.length;
-        for (var index = 1; index < length; ++index)
-        {
-            replacement += '+[' + encodeDigit(number[index]) + ']';
-        }
-        if (length > 1)
-        {
-            replacement = '+(' + replacement + ')';
-        }
-        if (total[offset - 1] === '+')
-        {
-            replacement = '(' + replacement + ')';
-        }
-        return replacement;
-    }
-    
-    function stringReplacer(quotedString, string, offset, total)
-    {
-        var result =
-            this.resolveString(
-            string,
-            /[+!]/.test(total[offset - 1]) || total[offset + quotedString.length] === '['
-            );
-        return result;
     }
     
     fillMissingDigits();
@@ -598,7 +643,7 @@
     
     function encode(input, wrapWithEval, compatibility)
     {
-        compatibility = compatibility === undefined ? 'DEFAULT' : String(compatibility);
+        compatibility = compatibility === undefined ? 'DEFAULT' : compatibility + '';
         var encoder = encoders[compatibility];
         if (!encoder)
         {
