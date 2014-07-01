092db8356eba47c2835584d314480377818b39dd
Francesco Trotta <github@fasttime.org>
2014-07-01 07:27:50 +0200

    More work on the Jasmine test suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,81 @@
-/*global module:false*/
-module.exports = function(grunt) {
+/* global module: false */
+module.exports =
+    function (grunt)
+    {
+        // Project configuration.
+        grunt.initConfig(
+            {
+                // Task configuration.
+                jasmine_node:
+                {
+                    options:
+                    {
+                        forceExit: true
+                    },
+                    all: ['jasmine/']
+                },
+                jshint:
+                {
+                    options:
+                    {
+                        curly: true,
+                        eqeqeq: true,
+                        immed: true,
+                        newcap: true,
+                        noarg: true,
+                        quotmark: true,
+                        sub: true,
+                        trailing: true,
+                        undef: true,
+                        unused: true,
+                        
+                        boss: true,
+                        eqnull: true,
+                        evil: true,
+                        validthis: true,
+                        
+                        node: true,
+                        globals:
+                        {
+                            window: true,
+                        }
+                    },
+                    gruntfile:
+                    {
+                        src: 'Gruntfile.js'
+                    },
+                    lib_test:
+                    {
+                        src: ['jscrewit.js', 'lib/**/*.js', 'test/**/*.js']
+                    }
+                },
+                nodeunit:
+                {
+                    files: ['test/**/*_test.js']
+                },
+                watch:
+                {
+                    gruntfile:
+                    {
+                        files: '<%= jshint.gruntfile.src %>',
+                        tasks: ['jshint:gruntfile']
+                    },
+                    lib_test:
+                    {
+                        files: '<%= jshint.lib_test.src %>',
+                        tasks: ['jshint:lib_test', 'nodeunit']
+                    }
+                }
+            }
+        );
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        newcap: true,
-        noarg: true,
-        quotmark: true,
-        sub: true,
-        trailing: true,
-        undef: true,
-        unused: true,
-        
-        boss: true,
-        eqnull: true,
-        evil: true,
-        validthis: true,
-        
-        node: true,
-        globals: {
-          window: true,
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['jscrewit.js', 'lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
-      }
-    }
-  });
+        // These plugins provide necessary tasks.
+        grunt.loadNpmTasks('grunt-contrib-nodeunit');
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-jasmine-node');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        // Default task.
+        grunt.registerTask('default', ['jshint', 'nodeunit', 'jasmine_node']);
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
-
-};
+    };
---
+++ jasmine/jscrewit_spec.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var exports = require('../jscrewit.js');
+var TestSuite = require('./test_suite.js').TestSuite;
+TestSuite.init(exports);
+TestSuite.run();
