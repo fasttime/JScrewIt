0f86c23f58120bc9da75f1d406e69ee4c89f584d
Francesco Trotta <github@fasttime.org>
2014-07-08 09:52:53 +0200

    Changed ☺ into ♥ in lengths output
    The heart sign is better supported by monospace fonts than the happy
    emoticon and it’s more popular.

--- jasmine/test_suite.js
+++ jasmine/test_suite.js
@@ -43,8 +43,8 @@
         }
         result += '\n`©`';
         appendLengths('©');
-        result += '\n`☺`';
-        appendLengths('☺');
+        result += '\n`♥`';
+        appendLengths('♥');
         return result;
     }
     
