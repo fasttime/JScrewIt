80683a8414b76e3b9a6e752e7c797af190646848
Francesco Trotta <github@fasttime.org>
2014-09-10 17:40:28 +0200

    Improved coverage

--- jasmine/jscrewit_spec.js
+++ jasmine/jscrewit_spec.js
@@ -9,38 +9,38 @@ console.log('Available features: ' + TestSuite.getAvailableFeatures().join(', ')
 TestSuite.run();
 
 var output =
-    (function ()
+(function ()
+{
+    try
     {
-        try
+        global.atob =
+        function (value)
         {
-            global.atob =
-            function (value)
-            {
-                return new Buffer(value + '', 'base64').toString('binary');
-            };
-            
-            global.btoa =
-            function (value)
-            {
-                return new Buffer(value + '', 'binary').toString('base64');
-            };
-            
-            global.self =
-            {
-                toString: function () { return '[object Window]'; }
-            };
-            
-            var result =
-                TestSuite.createOutput(['DEFAULT', 'COMPACT', 'NO_IE', 'FF31', 'IE11', 'NODE']);
-            return result;
-        }
-        finally
+            return new Buffer(value + '', 'base64').toString('binary');
+        };
+        
+        global.btoa =
+        function (value)
         {
-            delete global.atob;
-            delete global.btoa;
-            delete global.self;
-        }
+            return new Buffer(value + '', 'binary').toString('base64');
+        };
+        
+        global.self =
+        {
+            toString: function () { return '[object Window]'; }
+        };
+        
+        var result =
+            TestSuite.createOutput(['DEFAULT', 'COMPACT', 'NO_IE', 'FF31', 'IE11', 'NODE']);
+        return result;
     }
-    )();
+    finally
+    {
+        delete global.atob;
+        delete global.btoa;
+        delete global.self;
+    }
+}
+)();
 
 fs.writeFile('output.txt', output);
