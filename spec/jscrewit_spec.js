1bacf249881e290c4d59f00d042c05f9ffbe1a6d
Francesco Trotta <github@fasttime.org>
2014-10-25 10:56:23 +0200

    Definition oriented tests and minor fixes

--- spec/jscrewit_spec.js
+++ spec/jscrewit_spec.js
@@ -8,39 +8,5 @@ TestSuite.init(JScrewIt);
 console.log('Available features: ' + TestSuite.getAvailableFeatures().join(', '));
 TestSuite.run();
 
-var output =
-(function ()
-{
-    try
-    {
-        global.atob =
-        function (value)
-        {
-            return new Buffer(value + '', 'base64').toString('binary');
-        };
-        
-        global.btoa =
-        function (value)
-        {
-            return new Buffer(value + '', 'binary').toString('base64');
-        };
-        
-        global.self =
-        {
-            toString: function () { return '[object Window]'; }
-        };
-        
-        var result =
-            TestSuite.createOutput(['DEFAULT', 'COMPACT', 'NO_IE', 'FF31', 'IE11', 'NODE']);
-        return result;
-    }
-    finally
-    {
-        delete global.atob;
-        delete global.btoa;
-        delete global.self;
-    }
-}
-)();
-
+var output = TestSuite.createOutput(['DEFAULT', 'COMPACT', 'NO_IE', 'FF31', 'IE11', 'NODE']);
 fs.writeFile('output.txt', output);
