1ea465f08c27c5e87121317bfbfe629f3d4f0340
Francesco Trotta <github@fasttime.org>
2014-12-24 19:13:52 +0100

    Cleaning coverage folder and .min.js before build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,12 @@ function (grunt)
     // Project configuration.
     grunt.initConfig(
         {
+            clean: ['coverage', 'lib/**/*.min.js'],
             jasmine_node:
-			{
-				options: { },
-				src: ['**/*.js']
-			},
+            {
+                main: '**/*.js',
+                options: { }
+            },
             jshint:
             {
                 any:
@@ -47,17 +48,18 @@ function (grunt)
             },
             uglify:
             {
-                main: { files: { 'lib/jscrewit.min.js': ['lib/jscrewit.js'] } },
+                main: { files: { 'lib/jscrewit.min.js': 'lib/jscrewit.js' } },
                 options: { compress: { global_defs: { DEBUG: false } } }
             }
         }
     );
     
     // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-jasmine-node-coverage');
     
     // Default task.
-    grunt.registerTask('default', ['jshint', 'jasmine_node', 'uglify']);
+    grunt.registerTask('default', ['clean', 'jshint', 'jasmine_node', 'uglify']);
 };
