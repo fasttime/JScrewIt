a35c29479e96503eda08b56f75f9be5a4eacb830
Francesco Trotta <github@fasttime.org>
2015-03-30 01:03:37 +0200

    Updated grunt-jscs dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,6 @@ module.exports =
                         requireSpaceAfterKeywords: true,
                         requireSpaceAfterLineComment: true,
                         requireSpaceBeforeBinaryOperators: true,
-                        requireSpaceBeforeBlockStatements: true,
                         requireSpaceBeforeKeywords:
                         [
                             'delete',
