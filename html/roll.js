f6dd144192f60bfddbbe71509a607c24d49035c5
Francesco Trotta <github@fasttime.org>
2015-07-09 18:54:58 +0200

    New roll component

---
+++ html/roll.js
@@ -0,0 +1,64 @@
+/* exported createRoll */
+/* jshint browser: true */
+
+function createRoll()
+{
+    'use strict';
+    
+    function progress()
+    {
+        var now = +new Date();
+        var elapsed = now - startTime;
+        opening = startOpening + elapsed * openSign / 500;
+        if ((opening - endOpening) * openSign >= 0)
+        {
+            opening = endOpening;
+            stop();
+        }
+        style.height = comp.scrollHeight * opening + 'px';
+    }
+    
+    function rollTo(newEndOpening)
+    {
+        if (newEndOpening === opening)
+        {
+            stop();
+        }
+        else
+        {
+            var newOpenSign = newEndOpening > opening ? 1 : -1;
+            if (newOpenSign !== openSign)
+            {
+                startOpening = opening;
+                startTime = +new Date();
+                openSign = newOpenSign;
+            }
+            endOpening = newEndOpening;
+            if (!interval)
+            {
+                interval = setInterval(progress, 0);
+            }
+        }
+    }
+    
+    function stop()
+    {
+        clearInterval(interval);
+        interval = null;
+        openSign = 0;
+    }
+    
+    var comp = document.createElement('DIV');
+    Object.defineProperty(comp, 'rollTo', { configurable: true, value: rollTo, writable: true });
+    var style = comp.style;
+    style.height = '0';
+    style.overflowY = 'hidden';
+    var opening = 0;
+    var openSign = 0;
+    var startOpening;
+    var endOpening;
+    var startTime;
+    var interval;
+    
+    return comp;
+}
