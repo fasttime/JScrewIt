278314651b0178a6251f4a2b28c536ae4e138c37
Francesco Trotta <github@fasttime.org>
2015-08-02 21:53:36 +0200

    Added Analyzer

---
+++ build/analyzer.js
@@ -0,0 +1,155 @@
+/* global module, require, self */
+
+(function ()
+{
+    'use strict';
+    
+    function FeatureQueryInfo(featureMask, included, ancestorFeatureMask)
+    {
+        this.featureMask = featureMask;
+        this.included = included;
+        this.ancestorFeatureMask = ancestorFeatureMask;
+    }
+    
+    function createModifiedEncoder(features, featureQueries)
+    {
+        function hasFeatures(featureMask)
+        {
+            var included = (featureMask & this.featureMask) === featureMask;
+            if (featureMask)
+            {
+                if (!featureMaskSet[featureMask])
+                {
+                    featureMaskSet[featureMask] = true;
+                    var featureQuery =
+                        new FeatureQueryInfo(featureMask, included, ancestorFeatureMask);
+                    featureQueries.push(featureQuery);
+                }
+            }
+            if (included)
+            {
+                ancestorFeatureMask |= featureMask;
+            }
+            return included;
+        }
+        
+        var featureMaskSet = Object.create(null);
+        var ancestorFeatureMask = 0;
+        var encoder = JScrewIt.debug.createEncoder(features);
+        encoder.hasFeatures = hasFeatures;
+        return encoder;
+    }
+    
+    function getNewFeatureData(analyzer)
+    {
+        var featureQueries = analyzer.featureQueries;
+        if (!featureQueries)
+        {
+            return true;
+        }
+        for (var index = featureQueries.length; index--;)
+        {
+            var featureQuery = featureQueries[index];
+            if (!featureQuery.included)
+            {
+                var featureMask = featureQuery.featureMask | featureQuery.ancestorFeatureMask;
+                if (isIndependentFeatureMask(featureQueries, index, featureMask))
+                {
+                    var features = JScrewIt.debug.featuresFromMask(featureMask);
+                    if (JScrewIt.areFeaturesCompatible(features))
+                    {
+                        analyzer.features = features;
+                        analyzer.featureMask = featureMask;
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    
+    function getProgress(featureQueries)
+    {
+        var step = 1;
+        var progress = 0;
+        featureQueries.forEach(
+            function (featureQuery)
+            {
+                step /= 2;
+                if (featureQuery.included)
+                {
+                    progress += step;
+                }
+            }
+        );
+        progress += step;
+        return progress;
+    }
+    
+    function isIndependentFeatureMask(featureQueries, index, newFeatureMask)
+    {
+        while (index--)
+        {
+            var featureQuery = featureQueries[index];
+            if (!featureQuery.included)
+            {
+                var featureMask = featureQuery.featureMask;
+                if ((featureMask & newFeatureMask) === featureMask)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    function Analyzer()
+    {
+        this.features = [];
+        this.featureMask = 0;
+    }
+    
+    var JScrewIt;
+    
+    Object.defineProperties(
+        Analyzer.prototype,
+        {
+            'nextEncoder':
+            {
+                configurable: true,
+                get: function ()
+                {
+                    if (!getNewFeatureData(this))
+                    {
+                        return null;
+                    }
+                    var featureQueries = this.featureQueries = [];
+                    var encoder = createModifiedEncoder(this.features, featureQueries);
+                    return encoder;
+                }
+            },
+            'progress':
+            {
+                configurable: true,
+                get: function ()
+                {
+                    var featureQueries = this.featureQueries;
+                    var progress = featureQueries ? getProgress(featureQueries) : 0;
+                    return progress;
+                }
+            }
+        }
+    );
+    
+    if (typeof self !== 'undefined')
+    {
+        JScrewIt = self.JScrewIt;
+        self.Analyzer = Analyzer;
+    }
+    
+    if (typeof module !== 'undefined')
+    {
+        JScrewIt = require('../lib/jscrewit.js');
+        module.exports = Analyzer;
+    }
+}
+)();
