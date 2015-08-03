a634cbcec161bc238632e545fc4bb11440508d60
Francesco Trotta <github@fasttime.org>
2015-08-03 12:36:52 +0200

    Updated atob encoding definitions

---
+++ build/def-verifier.js
@@ -0,0 +1,56 @@
+/* global module, require, self */
+
+(function ()
+{
+    'use strict';
+    
+    function verifyDefinitions(entries, inputList, mismatchCallback)
+    {
+        function considerInput(input)
+        {
+            var solution = encoder.replaceString(input);
+            var length = solution.length;
+            if (length <= optimalLength)
+            {
+                if (length < optimalLength)
+                {
+                    optimalInputs = Object.create(null);
+                    optimalLength = length;
+                }
+                optimalInputs[input] = true;
+            }
+        }
+        
+        var analyzer = new Analyzer();
+        var encoder;
+        while (encoder = analyzer.nextEncoder)
+        {
+            var optimalInputs = null;
+            var optimalLength = Infinity;
+            inputList.forEach(considerInput);
+            analyzer.stopCapture();
+            var actualDefinition = encoder.findBestDefinition(entries);
+            if (!optimalInputs[actualDefinition])
+            {
+                var features = analyzer.features;
+                var expectedDefinitions = Object.keys(optimalInputs).sort();
+                mismatchCallback(features, actualDefinition, expectedDefinitions);
+            }
+        }
+    }
+    
+    var Analyzer;
+    
+    if (typeof self !== 'undefined')
+    {
+        Analyzer = self.Analyzer;
+        self.verifyDefinitions = verifyDefinitions;
+    }
+    
+    if (typeof module !== 'undefined')
+    {
+        Analyzer = require('./analyzer.js');
+        module.exports = verifyDefinitions;
+    }
+}
+)();
