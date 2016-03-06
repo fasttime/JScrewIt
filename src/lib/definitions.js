791d716021c9637039490d48f0f98b8db17a901f
Francesco Trotta <github@fasttime.org>
2016-03-06 20:29:20 +0100

    Fixed NO_V8_SRC paddings

--- src/lib/definitions.js
+++ src/lib/definitions.js
@@ -226,7 +226,7 @@ var replaceDigit;
                 paddingEntries =
                 [
                     define(12),
-                    define({ block: 'RP_2_SO', indexer: '2 + FH_SHIFT_1' }, 'NO_V8_SRC'),
+                    define({ block: 'RP_0_S', indexer: '2 + FH_SHIFT_3' }, 'NO_V8_SRC'),
                     define(3, 'V8_SRC'),
                     define(0, 'IE_SRC'),
                     define(0, 'NO_IE_SRC', 'NO_V8_SRC')
@@ -237,7 +237,10 @@ var replaceDigit;
                 paddingEntries =
                 [
                     define(10),
-                    define({ block: 'RP_0_S', indexer: index / 10 + ' + FH_SHIFT_1' }, 'NO_V8_SRC'),
+                    define(
+                        { block: 'RP_6_SO', indexer: 1 + index / 10 + ' + FH_SHIFT_1' },
+                        'NO_V8_SRC'
+                    ),
                     define(0, 'V8_SRC'),
                     define(5, 'IE_SRC'),
                     define(6, 'NO_IE_SRC', 'NO_V8_SRC')
@@ -247,7 +250,7 @@ var replaceDigit;
                 paddingEntries =
                 [
                     define(7),
-                    define({ block: 'RP_7_SO', indexer: '3 + FH_SHIFT_1' }, 'NO_V8_SRC'),
+                    define({ block: 'RP_3_NO', indexer: '3 + FH_SHIFT_1' }, 'NO_V8_SRC'),
                     define(0, 'V8_SRC'),
                     define(3, 'IE_SRC'),
                     define(3, 'NO_IE_SRC', 'NO_V8_SRC')
@@ -258,7 +261,7 @@ var replaceDigit;
                 [
                     define(7),
                     define(5, 'NO_IE_SRC'),
-                    define({ block: 'RP_5_N', indexer: '3 + FH_SHIFT_1' }, 'NO_V8_SRC'),
+                    define({ block: 'RP_1_NO', indexer: '3 + FH_SHIFT_1' }, 'NO_V8_SRC'),
                     define(0, 'IE_SRC'),
                     define(1, 'NO_IE_SRC', 'NO_V8_SRC')
                 ];
@@ -268,7 +271,7 @@ var replaceDigit;
                 [
                     define(8),
                     define(9, 'NO_IE_SRC'),
-                    define({ block: 'RP_0_S', indexer: '3 + FH_SHIFT_3' }, 'NO_V8_SRC'),
+                    define({ block: 'RP_4_N', indexer: '4 + FH_SHIFT_1' }, 'NO_V8_SRC'),
                     define(0, 'V8_SRC'),
                     define(3, 'IE_SRC'),
                     define(4, 'NO_IE_SRC', 'NO_V8_SRC')
@@ -279,7 +282,7 @@ var replaceDigit;
                 [
                     define(7),
                     define(9, 'NO_IE_SRC'),
-                    define({ block: 'RP_6_SO', indexer: '4 + FH_SHIFT_1' }, 'NO_V8_SRC'),
+                    define({ block: 'RP_2_SO', indexer: '4 + FH_SHIFT_1' }, 'NO_V8_SRC'),
                     define(6, 'V8_SRC'),
                     define(1, 'IE_SRC'),
                     define(3, 'NO_IE_SRC', 'NO_V8_SRC')
@@ -1012,7 +1015,9 @@ var replaceDigit;
         [
             define('Object["name"]', 'NAME'),
             define(undefined, 'CAPITAL_HTML', 'NO_IE_SRC', 'SELF_OBJ'),
-            define('Object["name"]', 'NAME', 'NO_IE_SRC', 'NO_V8_SRC'),
+            define(undefined, 'CAPITAL_HTML', 'FILL', 'NO_V8_SRC', 'SELF_OBJ'),
+            define('Object["name"]', 'CAPITAL_HTML', 'FILL', 'IE_SRC', 'NAME', 'SELF_OBJ'),
+            define('Object["name"]', 'NAME', 'NO_V8_SRC'),
             define('Object["name"]', 'INTL', 'NAME'),
             define('Object["name"]', 'NAME', 'V8_SRC'),
             define(undefined, 'ENTRIES_OBJ')
@@ -1203,8 +1208,6 @@ var replaceDigit;
         RP_4_N:         'true',
         RP_5_N:         'false',
         RP_6_SO:        '"0false"',
-        RP_7_SO:        '"NaNtrue"',
-        RP_9_U:         'undefined',
     });
     
     var createParseIntArgByReduce =
