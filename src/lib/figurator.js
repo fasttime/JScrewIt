31cd0c6132c502c8e561b373867bb35cfa13f477
Francesco Trotta <github@fasttime.org>
2016-12-31 13:39:17 +0100

    Improved dense figure legend encoding

--- src/lib/figurator.js
+++ src/lib/figurator.js
@@ -1,6 +1,6 @@
 /* global Empty, array_prototype_push */
 
-function createFigurator(protoFigures)
+function createFigurator(startValues, joiner)
 {
     function createFigure(value, sortLength)
     {
@@ -9,11 +9,18 @@ function createFigurator(protoFigures)
         return figure;
     }
     
+    function createPart(value, sortLength, isJoiner)
+    {
+        var part = createFigure(value, sortLength);
+        part.isJoiner = isJoiner;
+        return part;
+    }
+    
     function figurator(index)
     {
         while (figures.length <= index)
         {
-            parts.forEach(growFigures);
+            appendableParts.forEach(growFigures);
             var newFigures = figureList[currentSortLength++];
             if (newFigures)
                 array_prototype_push.apply(figures, newFigures);
@@ -32,13 +39,13 @@ function createFigurator(protoFigures)
                 function (oldFigure)
                 {
                     var newValue = oldFigure + part;
-                    pushFigure(newValue, currentSortLength);
+                    pushFigure(newValue, currentSortLength, part);
                 }
             );
         }
     }
     
-    function pushFigure(value, sortLength)
+    function pushFigure(value, sortLength, part)
     {
         if (!(value in usedValueSet))
         {
@@ -46,55 +53,68 @@ function createFigurator(protoFigures)
             var figures = figureList[sortLength] || (figureList[sortLength] = []);
             var figure = createFigure(value, sortLength);
             figures.push(figure);
+            part.isJoiner = false;
+            for (; ; ++joinerIndex)
+            {
+                var joinerPart = PARTS[joinerIndex];
+                if (!joinerPart)
+                    break;
+                if (joinerPart.isJoiner)
+                {
+                    figure.joiner = joinerPart.valueOf();
+                    break;
+                }
+            }
         }
     }
+    
+    var PARTS =
+    [
+        createPart('',          0,  false),
+        createPart('false',     4,  true),
+        createPart('true',      5,  true),
+        createPart('0',         6,  true),
+        createPart('undefined', 7,  true),
+        createPart('1',         8,  true),
+        createPart('NaN',       9,  true),
+        createPart('2',         12, true),
+        createPart('f',         14, false),
+        createPart('t',         15, false),
+        createPart('a',         16, false),
+        createPart('3',         17, true),
+        createPart('N',         17, false),
+        createPart('r',         17, false),
+        createPart('u',         17, false),
+        createPart('n',         19, false),
+        createPart('l',         20, false),
+        createPart('4',         22, true),
+        createPart('d',         23, false),
+        createPart('s',         25, false),
+        createPart('e',         26, false),
+        createPart('5',         27, true),
+        createPart('i',         28, false),
+        createPart('6',         32, true),
+        createPart('7',         37, true),
+        createPart('8',         42, true),
+        createPart('9',         47, true),
+    ];
     
     var currentSortLength = 0;
     var figureList = [];
     var figures = [];
-    var parts =
-    [
-        createFigure('true', 5),
-        createFigure('0', 6),
-        createFigure('undefined', 7),
-        createFigure('1', 8),
-        createFigure('NaN', 9),
-        createFigure('2', 12),
-        createFigure('f', 14),
-        createFigure('t', 15),
-        createFigure('a', 16),
-        createFigure('3', 17),
-        createFigure('N', 17),
-        createFigure('r', 17),
-        createFigure('u', 17),
-        createFigure('n', 19),
-        createFigure('l', 20),
-        createFigure('4', 22),
-        createFigure('d', 23),
-        createFigure('s', 25),
-        createFigure('e', 26),
-        createFigure('5', 27),
-        createFigure('i', 28),
-        createFigure('6', 32),
-        createFigure('7', 37),
-        createFigure('8', 42),
-        createFigure('9', 47),
-    ];
+    var joinerIndex = 0;
     var usedValueSet = new Empty();
-    
-    protoFigures.forEach(
-        function (protoFigure)
-        {
-            pushFigure(protoFigure.value, protoFigure.sortLength);
-        }
-    );
-    parts =
-        parts.filter(
+    var appendableParts =
+        PARTS.filter(
             function (part)
             {
-                var result = !(part in usedValueSet);
-                return result;
+                var value = part.valueOf();
+                if (startValues.indexOf(value) >= 0)
+                    pushFigure(value, part.sortLength, part);
+                else if (value !== joiner)
+                    return true;
             }
         );
+    
     return figurator;
 }
