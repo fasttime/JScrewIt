61202f49683c62aa939f9d302e5300a568d16661
Francesco Trotta <github@fasttime.org>
2017-01-21 17:41:26 +0100

    Split lib spec
    Also changed Feature#check to always return a boolean.

---
+++ test/lib/solution.spec.js
@@ -0,0 +1,69 @@
+/* eslint-env mocha */
+/* global expect, module, require, self */
+
+(function ()
+{
+    'use strict';
+    
+    var JScrewIt = typeof module !== 'undefined' ? require('../node-jscrewit-test') : self.JScrewIt;
+    
+    describe(
+        'Solution#hasOuterPlus is',
+        function ()
+        {
+            it(
+                'true for leading plus',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('+[]');
+                    expect(solution.hasOuterPlus).toBe(true);
+                }
+            );
+            it(
+                'true for middle plus',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('[]+[]');
+                    expect(solution.hasOuterPlus).toBe(true);
+                }
+            );
+            it(
+                'false for inner plus',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('(+[])');
+                    expect(solution.hasOuterPlus).toBe(false);
+                }
+            );
+            it(
+                'false for leading !+',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('!+[]');
+                    expect(solution.hasOuterPlus).toBe(false);
+                }
+            );
+            it(
+                'cached upon creation',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('', void 0, true);
+                    expect(solution.hasOwnProperty('hasOuterPlus')).toBeTruthy();
+                    expect(solution.hasOuterPlus).toBe(true);
+                }
+            );
+            it(
+                'cached upon first access',
+                function ()
+                {
+                    var solution = JScrewIt.debug.createSolution('+');
+                    expect(solution.hasOwnProperty('hasOuterPlus')).toBeFalsy();
+                    void solution.hasOuterPlus;
+                    expect(solution.hasOwnProperty('hasOuterPlus')).toBeTruthy();
+                    expect(solution.hasOuterPlus).toBe(true);
+                }
+            );
+        }
+    );
+}
+)();
