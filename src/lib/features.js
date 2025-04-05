67ae43eafc951aa00257764655c6a78be6455a2b
Clay <clay0305@hotmail.com>
2025-04-05 10:15:28 +0800

    test end

--- src/lib/features.js
+++ src/lib/features.js
@@ -635,6 +635,8 @@ var featureInfos =
             return available;
         },
         includes: ['LOCALE_NUMERALS'],
+        excludes: ['LOCALE_NUMERALS_EXT'],
+
     },
     LOCALE_NUMERALS_IE11_WIN8:
     {
